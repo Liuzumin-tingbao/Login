bbd5fd71efca19e56c8a7c93f919fe8613cd3052
Liuzumin-tingbao <1016798559@qq.com>
2019-10-18 20:52:06 +0800

    second commit

--- src/test/java/Login/write/quit/Login.java
+++ src/test/java/Login/write/quit/Login.java
@@ -7,44 +7,34 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Login {
     WebDriver wb;
+    @DataProvider(name = "TestLogin")
+    public Object[][] data1(){
+        return new Object[][]{{"aaa@qq.com","pwd111"}};
+    }
     @BeforeMethod
     public void OpenChrome() throws InterruptedException{
         System.setProperty("webdriver.chrome.driver","E:\\IDEAProject\\drivers\\chromedriver.exe");
         wb = new ChromeDriver();
-        wb.get("http://127.0.0.1:5000/login");
-        wb.findElement(By.xpath("/html/body/section/div/div/div/form/p[4]/a")).click();
-        Thread.sleep(2000);
-        wb.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("124@qq.com");
-        wb.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456");
-        wb.findElement(By.xpath("//*[@id=\"cpassword\"]")).sendKeys("123456");
-        wb.findElement(By.xpath("/html/body/section/div/div/div/form/p[2]/input")).click();
     }
-    @Test
-    public void LoginWeb() throws InterruptedException {
+    @Test(dataProvider = "TestLogin")
+    public void LoginWeb(String a,String b) throws InterruptedException {
+        //注册
+        Fuction.Register(wb, a,b);
+        Thread.sleep(3000);
         //登陆
-        wb.get("http://127.0.0.1:5000/login");
-        wb.findElement(By.id("email")).sendKeys("124@qq.com");
-        wb.findElement(By.id("password")).sendKeys("123456");
-        wb.findElement(By.xpath("/html/body/section/div/div/div/form/p[3]/input")).click();
+        Fuction.LoginTest(wb,a,b);
         //发帖
         Thread.sleep(2000);
 
-        wb.findElement(By.xpath("/html/body/header/div/div/div/strong/a")).click();
-        wb.findElement(By.xpath("//*[@id=\"new_post_link\"]")).click();
-
+        Fuction.PostTest(wb,a,b);
         Thread.sleep(3000);
-        wb.findElement(By.xpath("//*[@id=\"title\"]")).sendKeys("1234");
-        wb.findElement(By.xpath("//*[@id=\"slug\"]")).sendKeys("1234");
-        wb.findElement(By.xpath("//*[@id=\"body\"]")).sendKeys("123456767889");
-        wb.findElement(By.xpath("//*[@id=\"new_post_form\"]/div/form/p[2]/input")).click();
-        Thread.sleep(3000);
-
-        wb.findElement(By.xpath("/html/body/header/div/div/div/a[2]")).click();
-        Thread.sleep(2000);
+        //退出
+        Fuction.LogoutTest(wb);
     }
     @AfterMethod
     public void CloseChrome(){
